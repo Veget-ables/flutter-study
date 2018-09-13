d0f3fdb4cba7bd7b119d46d9c076be359040f7c5
TakakiTojo <makuhari109@gmail.com>
2018-09-13 22:18:47 +0900

    done building layout in flitter gridview

---
+++ lib/layout/gridview.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+Widget buildGrid() {
+  return GridView.extent(
+    maxCrossAxisExtent: 150.0,
+    padding: const EdgeInsets.all(4.0),
+    mainAxisSpacing: 4.0,
+    crossAxisSpacing: 4.0,
+    children: _buildGridTitleList(6),
+  );
+}
+
+List<Container> _buildGridTitleList(int count) {
+  List<Container> containers = List<Container>.generate(count,
+      (int index) => Container(child: Image.asset('images/pic${index+1}.jpg'))
+  );
+  return containers;
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+       child: buildGrid(),
+      ),
+    );
+  }
+}
